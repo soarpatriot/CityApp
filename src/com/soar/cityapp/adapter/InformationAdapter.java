16a33de8e39e130e396f25fb3313ce9e17c6b456
soar <85624529@qq.com>
2014-11-05 17:43:16 +0800

    change layout

--- src/com/soar/cityapp/adapter/InformationAdapter.java
+++ src/com/soar/cityapp/adapter/InformationAdapter.java
@@ -37,24 +37,32 @@ public class InformationAdapter extends BaseAdapter{
             if(convertView==null){  
                 convertView=LayoutInflater.from(mContext).inflate(R.layout.item, null);  
                 ItemViewCache viewCache=new ItemViewCache();  
-                viewCache.mTextView=(TextView)convertView.findViewById(R.id.text);  
+                viewCache.titleTextView = (TextView) convertView.findViewById(R.id.title); 
+                viewCache.descriptionTextView = (TextView) convertView.findViewById(R.id.description);
+                viewCache.timeAgoTextView = (TextView) convertView.findViewById(R.id.time_ago); 
                 viewCache.mImageView=(ImageView)convertView.findViewById(R.id.image);  
                 viewCache.mImageView.setScaleType(ScaleType.CENTER_CROP);
                 convertView.setTag(viewCache);  
             }  
             ItemViewCache cache=(ItemViewCache)convertView.getTag();  
             //设置文本和图片，然后返回这个View，用于ListView的Item的展示  
-            cache.mTextView.setText(texts[position]);  
+            cache.titleTextView.setText(texts[position]);  
+            cache.descriptionTextView.setText(desc[position]);  
+            cache.timeAgoTextView.setText(timeAgo[position]);  
             cache.mImageView.setImageResource(images[position]);  
             return convertView;  
         }  
     
     //元素的缓冲类,用于优化ListView  
     private static class ItemViewCache{  
-        public TextView mTextView;  
+        public TextView titleTextView;  
+        public TextView descriptionTextView;  
+        public TextView timeAgoTextView;  
         public ImageView mImageView;  
     }  
     private  String[] texts=new String[]{"天气","我团","背景"};  
+    private  String[] desc=new String[]{"我的","你的","他的"};  
+    private  String[] timeAgo=new String[]{"一天前","两天前","3天前"}; 
     //展示的图片  
     private int[] images=new int[]{R.drawable.no_scrip,R.drawable.no_scrip,R.drawable.no_scrip};  
 
